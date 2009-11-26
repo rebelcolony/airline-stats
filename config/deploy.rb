62ad27e8fbea34103af36e3b771a041b088cc80f
Alastair Brunton <info@simplyexcited.co.uk>
2009-11-26 15:36:25 +0100

    further deployment tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
 set :application, "airline-stats"
 set :repository,  "git@github.com:rebelcolony/airline-stats.git"
+set :rails_env, "production"
 
 set :scm, :git
 set :scm_verbose, true
@@ -19,5 +20,14 @@ set :user, root
 ssh_options[:keys] = %w(/Users/alastairbrunton/.ssh/rebel)
 ssh_options[:port] = 22
 
-after "deploy:update_code", "recipiez:rename_db_file" 
+after "deploy:update_code", "recipiez:rename_db_file"
+before "deploy:symlink", "deploy:change_owner" 
 
+namespace :deploy do
+  task :restart do
+    recipiez::restart_passenger
+  end
+  task :change_owner do
+    run "chown -R nobody:nobody #{release_path}"
+  end
+end
