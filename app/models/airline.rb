89c221ae1f82122a525e49bdbfb68a86988f43ea
kevin bett <kevin@rebelcolony.com>
2009-11-28 15:27:22 +0800

    try to fix logo asset bug

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -3,4 +3,7 @@ class Airline < ActiveRecord::Base
   validates_length_of :name, :within => 2..15
   
     has_attached_file :logo, :styles => { :medium => "300x300>", :thumb => "80x80>" }
+  	validates_attachment_content_type :logo,
+  	                                    :content_type => ['image/jpeg', 'image/pjpeg', 
+  	                                                        'image/jpg', 'image/png']
 end
