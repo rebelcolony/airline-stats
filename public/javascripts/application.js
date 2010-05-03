a70024ce99c350d1abb65609932dd4b057aa3d6f
kevin bett <kevin@rebelcolony.com>
2010-05-03 06:44:15 +0600

    prep for top-up

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,9 @@
-jQuery(document).ready(function() {
-    jQuery('a[rel*=facebox]').facebox() 
-  })
+TopUp.images_path = "/images/top_up/";
+TopUp.addPresets({
+  "a.top_up": {
+    type: "dom",
+	x: 710,
+	y: 300,
+	width: 300
+  },
+});
